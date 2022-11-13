88aa5a407f4ebc3d56b5dfe5d6b3797624abb21f
Kelvin KUCH <engineerkuch@gmail.com>
2022-11-14 01:28:05 +0300

    One of the completed challanges while learning iOSDev with Angela on Udemy.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
     
     @IBAction func askButtonPressed(_ sender: Any) {
         imageView.image = ballArray.randomElement()
-//        imageView.image = ballArray[Int.random(in: 0 ... 4)]
     }
     
 }
